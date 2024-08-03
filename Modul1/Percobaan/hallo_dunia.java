dbc43ac4e0006ce88618d237e9206a42737bbb67
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-03 15:38:02 +0700

    Menyelesaikan Percobaan nomor 1

---
+++ Modul1/Percobaan/hallo_dunia.java
@@ -0,0 +1,11 @@
+package Modul1.Percobaan;
+
+public class hallo_dunia {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama : Moch. Minanur Rahman");
+        System.out.println("Gender : Laki-Laki");
+        System.out.println("Alamat : Jl. Peltu Sujono Gg. Sri Rejeki");
+        System.out.println("Tanggal Lahir : 29 Juli 2003 ");
+    }
+}
