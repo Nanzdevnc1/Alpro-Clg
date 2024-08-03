3abbd2ea06c185954420ae05d18d01a652f1ab1b
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-03 20:09:11 +0700

    Menyelesaikan pendahuluan nomor 3

---
+++ Modul2/Pendahuluan/Variabel.java
@@ -0,0 +1,15 @@
+package Modul2.Pendahuluan;
+
+public class Variabel {
+    public static void main(String[] args) {
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+
+        System.out.println(number);
+        System.out.println(letter);
+        System.out.println(result);
+        System.out.println(str);
+    }
+}
