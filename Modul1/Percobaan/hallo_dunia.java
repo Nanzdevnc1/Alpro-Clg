e6aeeb9931e7a1c4b236fe09e35e28adbd724a33
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-03 16:12:59 +0700

    Update hallo_dunia.java

--- Modul1/Percobaan/hallo_dunia.java
+++ Modul1/Percobaan/hallo_dunia.java
@@ -6,6 +6,6 @@ public class hallo_dunia {
         System.out.println("Nama : Moch. Minanur Rahman");
         System.out.println("Gender : Laki-Laki");
         System.out.println("Alamat : Jl. Peltu Sujono Gg. Sri Rejeki");
-        System.out.println("Tanggal Lahir : 29 Juli 2003 ");
+        System.out.println("Tanggal Lahir : 29 Juli 2003");
     }
 }
