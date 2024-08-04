ec19b062bded9b77714b7a99207f9678cfff5ea2
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-04 22:23:13 +0700

    Menambahkan Haji.java sekaligus menyelesaikannya

---
+++ Modul2/Percobaan/Haji.java
@@ -0,0 +1,15 @@
+package Modul2.Percobaan;
+
+public class Haji {
+    public static void main(String[] args) {
+        int biayaHaji = 50000000;
+        int bukaTabungan = 10000000;
+        byte sisaBulan = 23;
+        int sisaBiaya;
+        int cicilanHaji;
+
+        sisaBiaya = biayaHaji - bukaTabungan;
+        cicilanHaji = sisaBiaya / sisaBulan;
+        System.out.println("Sehingga minimal cicilan yang harus dibayarkan adalah " + cicilanHaji + " Rupiah");
+    }
+}
