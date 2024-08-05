88290920406138b9a4f184cf9afe298906e1196a
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-05 16:16:25 +0700

    Menyelesaikan tugas percobaan perpangkatan poin B

---
+++ src/Modul2/Percobaan/PangkatB.java
@@ -0,0 +1,18 @@
+package src.Modul2.Percobaan;
+
+public class PangkatB {
+    public static void main(String[] args) {
+        int nilaiPangkat1;
+        int nilaiPangkat2;
+        int nilaiPangkat3;
+        
+        int hasilAkhir;
+
+        nilaiPangkat1 = (int) Math.pow(2, 2);
+        nilaiPangkat2 = (int) Math.pow(4, 1);
+        nilaiPangkat3 = (int) Math.pow(2, 2);
+        
+        hasilAkhir = (nilaiPangkat1 + nilaiPangkat2) / nilaiPangkat3;
+        System.out.println("Hasil akhir dari (2^2 + 4^1) / 2^2 adalah " + hasilAkhir);
+    }
+}
