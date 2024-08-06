db708a5b5d3158e402e8140e37078a410b48f0d4
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-06 23:00:18 +0700

    Menyelesaikan tugas pendahuluan nomor 1

---
+++ src/Modul3/Pendahuluan/Nomor1.java
@@ -0,0 +1,24 @@
+package src.Modul3.Pendahuluan;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Nomor1 {
+    public static void main(String[] args) {
+
+        BufferedReader inputUser = new BufferedReader(new InputStreamReader(System.in));
+        int x, y, luas;
+
+        try {
+            System.out.print("Masukkan panjang : ");
+            x = Integer.parseInt(inputUser.readLine());
+            System.out.print("Masukkan lebar : ");
+            y = Integer.parseInt(inputUser.readLine());
+            luas = x * y;
+            System.out.println("Luas persegi panjang adalah " + luas);
+        } catch (Exception e) {
+            System.out.println("Error !");
+        } 
+    }
+}
