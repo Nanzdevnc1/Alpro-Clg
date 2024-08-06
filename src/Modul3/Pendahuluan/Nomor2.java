7400aae5d94cfa4cbb465be23e34b5f1d16417ea
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-06 23:17:43 +0700

    Update Nomor1.java dan menyelesaikan Nomor2.java

---
+++ src/Modul3/Pendahuluan/Nomor2.java
@@ -0,0 +1,15 @@
+package src.Modul3.Pendahuluan;
+
+import javax.swing.JOptionPane;
+
+public class Nomor2 {
+    public static void main(String[] args) {
+        String nama, pesan;
+        int umur;
+
+        nama = JOptionPane.showInputDialog("Masukkan nama Anda");
+        umur = Integer.parseInt(JOptionPane.showInputDialog("Masukkan umur Anda"));
+        pesan = "Terus berusahalah " + nama + " walaupun umur sudah " + umur + " karena 'the time is closing down'";
+        JOptionPane.showMessageDialog(null, pesan);
+    }
+}
