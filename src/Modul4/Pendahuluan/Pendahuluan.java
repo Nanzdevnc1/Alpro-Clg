934cc759a9a04593fcc9e551336d89a2f6017c91
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-08-17 13:57:19 +0700

    Menyelesaikan tugas pendahuluan

---
+++ src/Modul4/Pendahuluan/Pendahuluan.java
@@ -0,0 +1,26 @@
+package Modul4.Pendahuluan;
+
+public class Pendahuluan {
+    public static void main(String[] args) {
+        // While Loop
+        int x = 1;
+        while (x <= 10) {
+            System.out.println(x);
+            x++;
+        }
+        System.out.println();
+
+        // Do-While Loop
+        int y = 1;
+        do {
+            System.out.println(y);
+            y++;
+        } while (y <= 10);
+        System.out.println();
+
+        // For Loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
